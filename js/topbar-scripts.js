c3c2d3a5ac369d74a0c490a16229c7882e664392
ArsLibri <polichromindustry@yahoo.com>
2016-08-22 15:50:50 +0300

    Summary tree

--- js/topbar-scripts.js
+++ js/topbar-scripts.js
@@ -3,9 +3,9 @@ var currentpage = 1;
 
 var DOMtopbartoggle = document.getElementById('top-bar-toggle');
 var DOMtopbar = document.getElementById('top-bar');
-var DOMtopbarmenu = document.getElementById('top-bar-menu');
+var DOMsummary = document.getElementById('summary');
 
 DOMtopbartoggle.onclick = function() {
     menutoggled = !menutoggled;
-    DOMtopbarmenu.style.right = (-300 * !menutoggled) + 'px';
+    DOMsummary.style.left = (100 * !menutoggled) + '%';
 };
